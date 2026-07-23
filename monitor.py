d2e80fcae653d77b1175de5a3a174fb44f20dba1
ayyoubreguigue11-code <ayyoubreguigue11@gmail.com>
2026-07-23 02:47:04 +0100

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,1 +1,22 @@
+import os
+import requests
 
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+message = "✅ تم تشغيل نظام مراقبة نقط APoweb بنجاح."
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message,
+    },
+    timeout=30,
+)
+
+response.raise_for_status()
+print("Telegram notification sent successfully.")
